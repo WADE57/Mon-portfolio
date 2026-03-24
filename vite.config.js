1786ebed75d947637ca50959028f53a1e4871dca
WADE <abdouwade1996@gmail.com>
2026-03-24 01:40:06 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mon-portfolio/', // Remplacez par le nom de votre repo GitHub
+  base: '/Mon-portfolio/', // Remplacez par le nom de votre repo GitHub
 })
